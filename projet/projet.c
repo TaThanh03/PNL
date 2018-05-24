1eb5532a2b208d2fa41120ae3914a504df747f05
TATrungThanh <ta.thanh.03@gmail.com>
2018-05-24 11:34:49 +0200

    clean the code

--- projet/projet.c
+++ projet/projet.c
@@ -50,7 +50,7 @@ static struct inode *pnlfs_alloc_inode(struct super_block *sb)
 	//initialiser la struct inode qu’elle embarque avec la fonction inode_init_once() 
 	inode_init_once(&i->vfs_inode);
 	if (i)
-		pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) inode allocated\n");
+		pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_alloc_inode] inode allocated\n");
 	//renvoyer l’adresse de cette struct inode	
 	return i ? &i->vfs_inode : NULL;
 }
@@ -64,13 +64,12 @@ static void pnlfs_i_callback(struct rcu_head *head)
 static void pnl_destroy_inode(struct inode *inode)
 {
 	call_rcu(&inode->i_rcu, pnlfs_i_callback);
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) inode destroyed\n");
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnl_destroy_inode] inode destroyed\n");
 }
 
 static void pnlfs_put_super(struct super_block *sb)
 {	
 	//TODO mettre à jour sur disque ex10
-
 	//défaire ce que la fonction pnlfs_fill_super() a fait
 	kfree(sb->s_fs_info);
 	sb->s_fs_info = NULL;
@@ -91,9 +90,9 @@ static int pnlfs_readdir(struct file *file, struct dir_context *ctx)
 	uint32_t nr_ents;
 	int i, pos_start;
 
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) pnlfs_readdir directory #%lu file %pD2, pos=%d\n", i_dir->i_ino, file, (int)ctx->pos);
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_readdir] directory #%lu file %pD2, pos=%d\n", i_dir->i_ino, file, (int)ctx->pos);
 	if (!(i_dir->i_mode == S_IFDIR)){
-		pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) pnlfs_readdir Not a directory!!\n");
+		pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_readdir] Not a directory!!\n");
 		return -EINVAL;
 	}
 	if (ctx->pos < 0)
@@ -116,22 +115,26 @@ static int pnlfs_readdir(struct file *file, struct dir_context *ctx)
 	pos_start = ctx->pos - 2;
 	bh = sb_bread(i_dir->i_sb, dir_info->index_block);
 	if (!bh) 
-		pr_info(KERN_INFO "¯\\_(ツ)_/¯ unable to read directory %lu\n", i_dir->i_ino);
+		pr_info(KERN_INFO "¯\\_(ツ)_/¯ [pnlfs_readdir] unable to read directory %lu\n", i_dir->i_ino);
 	dir_block = (struct pnlfs_dir_block *) bh->b_data;
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) pnlfs_readdir nr_entries=%ld\n", (unsigned long)nr_ents);
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_readdir] nr_entries=%ld\n", (unsigned long)nr_ents);
 	for (i = pos_start; i < nr_ents; i++) {
-		dir_emit(ctx, dir_block->files[i].filename, strlen(dir_block->files[i].filename), le32_to_cpu(dir_block->files[i].inode), DT_UNKNOWN);
+		dir_emit(ctx, 
+			 dir_block->files[i].filename, 
+			 strlen(dir_block->files[i].filename), 
+			 le32_to_cpu(dir_block->files[i].inode), 
+			 DT_UNKNOWN);
 		ctx->pos++;
 	}
 	brelse(bh);
 	return 0;
 }
 
+//Return an inode coressond with the name we search for
 static ino_t pnlfs_inode_by_name(struct inode *dir, struct dentry *dentry)
 {
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) pnlfs_inode_by_name\n"); 
-  const char *name = dentry->d_name.name;
-  ino_t ino = 0; 
+	const char *name = dentry->d_name.name;
+	ino_t ino = 0; 
 	struct buffer_head *bh;
 	struct pnlfs_dir_block *dir_block;
 	struct pnlfs_inode_info *dir_info;
@@ -139,7 +142,7 @@ static ino_t pnlfs_inode_by_name(struct inode *dir, struct dentry *dentry)
 	
 	//lire directory meta-info
 	dir_info = PNLFS_I(dir); 
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) Directory infos (Inode infos):\n"
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_inode_by_name] Directory infos (Inode infos):\n"
 		"\ti_ino=%lu\n"
 		"\tindex_block=%u\n"
 		"\tnr_entries=%u\n",
@@ -149,12 +152,11 @@ static ino_t pnlfs_inode_by_name(struct inode *dir, struct dentry *dentry)
 	);
 	bh = sb_bread(dir->i_sb, dir_info->index_block);
 	if (!bh) 
-		pr_info(KERN_INFO "¯\\_(ツ)_/¯ unable to read directory %lu\n", dir->i_ino);
+		pr_info(KERN_INFO "¯\\_(ツ)_/¯ [pnlfs_inode_by_name] unable to read directory %lu\n", dir->i_ino);
 	dir_block = (struct pnlfs_dir_block *) bh->b_data;
 
-	//trouver le inode correspondant au nom
+	//trouver le inode correspond au nom
 	for (i = 0; i < dir_info->nr_entries; i++){
-		pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) %s\n", dir_block->files[i].filename); 
 		if (strcmp(dir_block->files[i].filename, name) == 0) {
 			ino = le32_to_cpu(dir_block->files[i].inode);
 			break;
@@ -183,11 +185,12 @@ lookup: called when the VFS needs to look up an inode in a parent
 	a pointer to an inode (d_inode), 
 	and a pointer to the parent dentry (d_parent).
 */
-static struct dentry *pnlfs_lookup(struct inode *dir, struct dentry *dentry, unsigned int flags)
+static struct dentry *pnlfs_lookup(struct inode *dir, struct dentry *dentry, 
+	unsigned int flags)
 {
 	ino_t ino;
-  struct inode *ip = NULL;
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) pnlfs_lookup\n"); 
+	struct inode *ip = NULL;
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_lookup]\n"); 
 	//get inode base on name
 	ino = pnlfs_inode_by_name(dir, dentry);
 	if (ino) {
@@ -198,7 +201,6 @@ static struct dentry *pnlfs_lookup(struct inode *dir, struct dentry *dentry, uns
 	d_add(dentry, ip);
 	return NULL;
 }
-
 
 static struct inode *pnlfs_get_free_inode(struct inode *dir)
 {
@@ -212,29 +214,76 @@ static struct inode *pnlfs_get_free_inode(struct inode *dir)
 	i_bitmap = pnlsb_info->ifree_bitmap;
 	pos = 0;	
 	for (i = 0; i < 32768; i++){		
-		if(i_bitmap[0] & (1 << i))
+		if(i_bitmap[0] & (1 << i)){
+			//change the bit in this possition to "0"	
+			i_bitmap[0] & ~(1 << i);
+			set_bit(i_bitmap, pos);
 			break;
+		}
 		pos++;
 	}
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) pnlfs_get_free_inode pos= %u\n", pos);
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_get_free_inode] pos= %u\n", pos);
  	fresh_inode = pnlfs_iget(dir->i_sb, pos);
 	return fresh_inode;
 }
 
-
-
+static void addin_directory(struct inode *dir, struct dentry *dentry, 
+	struct inode *fresh_inode, umode_t mode)
+{
+	int i;
+	struct buffer_head *bh;
+	struct pnlfs_dir_block *dir_block;
+	struct pnlfs_inode_info *dir_info;
+	//lire directory meta-info
+	dir_info = PNLFS_I(dir); 
+	bh = sb_bread(dir->i_sb, dir_info->index_block);
+	if (!bh) 
+		pr_info(KERN_INFO "¯\\_(ツ)_/¯ [addin_directory] unable to read directory %lu\n", dir->i_ino);
+	dir_block = (struct pnlfs_dir_block *) bh->b_data;
+	//+1 nombre de files/directories dans le repertoire 	
+	dir_info->nr_entries++;
+	//ajouter le nom
+	strcpy(dir_block->files[dir_info->nr_entries - 1].filename, dentry->d_name.name);
+	//ajouter l'inode
+	dir_block->files[dir_info->nr_entries - 1].inode = cpu_to_le32(fresh_inode->i_ino);
+}
 
 //create a regular file
-static int pnlfs_create(struct inode *dir, struct dentry *dentry, umode_t mode, bool excl)
+static int pnlfs_create(struct inode *dir, struct dentry *dentry, 
+	umode_t mode, bool excl)
 {
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_create]\n");
 	struct inode *fresh_inode;
 	//find a inode available by looking at "ifree_bitmap"
 	fresh_inode = pnlfs_get_free_inode(dir);
 	//add this new fresh inode and his "name" inside inode "dir"
-	
-	//if success, change the "1" in bitmap to "0"
-	
+	addin_directory(dir, dentry, fresh_inode, mode);
 	return 0;
+}
+
+static int pnlfs_unlink(struct inode *dir, struct dentry *dentry)
+{
+	int ret;
+	ino_t ino;
+	struct inode *ip = NULL;
+	//get inode base on name
+	ino = pnlfs_inode_by_name(dir, dentry);
+	if (ino) {
+		ip = pnlfs_iget(dir->i_sb, ino);
+		if (IS_ERR(ip))
+			return ERR_CAST(ip);
+	}
+	if (!ino){
+		pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_unlink] File not found!\n");
+		return 0;
+	}
+	//release this inode
+	
+	//remove from directory
+
+	//change the bitmap "0" to "1"
+
+	return ret;
 }
 
 
@@ -242,7 +291,7 @@ static int pnlfs_create(struct inode *dir, struct dentry *dentry, umode_t mode, 
 static const struct inode_operations pnlfs_inops = {
 	.lookup = pnlfs_lookup,
 	.create = pnlfs_create,
-	//.unlink = pnlfs_unlink,
+	.unlink = pnlfs_unlink,
 	//.mkdir  = pnlfs_mkdir,
 	//.rmdir  = pnlfs_rmdir,
  	//.rename = pnlfs_rename,
@@ -259,7 +308,7 @@ static struct inode *pnlfs_iget(struct super_block *sb, unsigned long ino)
 	struct pnlfs_inode_info *pnli;
 	struct pnlfs_inode *raw_inode;
 	struct buffer_head * bh;
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) inode %lu\n", ino);
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_iget] inode %lu\n", ino);
 	
 	//iget_locked appellera le constructeur pnlfs_alloc_inode
 	inode = iget_locked(sb, ino); 
@@ -280,7 +329,7 @@ static struct inode *pnlfs_iget(struct super_block *sb, unsigned long ino)
 	//Lire cette inode sur le disque
 	bh = sb_bread(sb, block_no); 
 	if (!bh) {
-		pr_info(KERN_INFO "¯\\_(ツ)_/¯ unable to read inode %lu\n", inode->i_ino);
+		pr_info(KERN_INFO "¯\\_(ツ)_/¯ [pnlfs_iget] unable to read inode %lu\n", inode->i_ino);
 		goto bad_inode;
 	}
 	raw_inode = (struct pnlfs_inode *) bh->b_data;
@@ -303,10 +352,10 @@ static struct inode *pnlfs_iget(struct super_block *sb, unsigned long ino)
 	//sauvegarder les metadonnees pour directory
 	if (inode->i_mode = S_IFDIR){
 		pnli->index_block = le32_to_cpu(raw_inode->index_block);  
-		pnli->nr_entries	= le32_to_cpu(raw_inode->nr_entries);
+		pnli->nr_entries  = le32_to_cpu(raw_inode->nr_entries);
 	}
 
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) Inode:\n"
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_iget] Inode:\n"
 		"\tinode->i_mode=%u\n"	
 		"\tinode->i_ino=%lu\n"
 		"\tinode->i_size=%lld\n"
@@ -322,29 +371,15 @@ static struct inode *pnlfs_iget(struct super_block *sb, unsigned long ino)
 	);
 
 	unlock_new_inode(inode);
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) new inode ok!\n");
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_iget] new inode ok!\n");
 	brelse(bh);
 	return inode;
+
 bad_inode:
 	iget_failed(inode);
 	return ERR_PTR(-EIO);
 }
 
-/*
-A fill_super() callback implementation has the following arguments:
-  struct super_block *sb: the superblock structure. The callback
-  	must initialize this properly.
-  void *data: arbitrary mount options, usually comes as an ASCII
-	string (see "Mount Options" section)
-  int silent: whether or not to be silent on error
-In general, data pointed to by the s_fs_info field is information from the disk duplicated in memory for
-reasons of efficiency. Each disk-based filesystem needs to access and update its allocation bitmaps in order to
-allocate or release disk blocks. The VFS allows these filesystems to act directly on the s_fs_info field of
-the superblock in memory without accessing the disk.
-This approach leads to a new problem, however: the VFS superblock might end up no longer synchronized
-with the corresponding superblock on disk. It is thus necessary to introduce an s_dirt flag, which specifies
-whether the superblock is dirtythat is, whether the data on the disk must be updated.
-*/
 static int pnlfs_fill_super(struct super_block *sb, void *d, int silent)
 {
 	struct inode *root;
@@ -371,12 +406,12 @@ static int pnlfs_fill_super(struct super_block *sb, void *d, int silent)
 	/*Lire ensuite le super bloc ( struct pnl_superblock dans pnlfs.h) 
 	sur le périphérique disque à l’aide de l’API buffer head*/
 	if (!(bh = sb_bread(sb, PNLFS_SB_BLOCK_NR))) { //Super block is the 0th block
-		pr_info(KERN_INFO "¯\\_(ツ)_/¯ unable to read superblock");
+		pr_info(KERN_INFO "¯\\_(ツ)_/¯ [pnlfs_fill_super] unable to read superblock");
 		ret = -EIO;
 		goto error;
 	}
 	memcpy(pnlsb, bh->b_data, PNLFS_BLOCK_SIZE);
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) Superblock: (%ld)\n"
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_fill_super] Superblock: (%ld)\n"
 		"\tmagic=%#x\n"
 		"\tnr_blocks=%u\n"
 		"\tnr_inodes=%u (istore=%u blocks)\n"
@@ -406,10 +441,10 @@ static int pnlfs_fill_super(struct super_block *sb, void *d, int silent)
 	pnlsb_info->bfree_bitmap = kmalloc(PNLFS_BLOCK_SIZE / 8 * pnlsb_info->nr_bfree_blocks * sizeof(uint64_t), GFP_KERNEL);
 	memset(pnlsb_info->bfree_bitmap, 0xff, PNLFS_BLOCK_SIZE);
 
-	//we don't know how to copy ifree_bitmap from disk so we do this :(
+	//we don't know how to copy ifree_bitmap from disk so we do this 
 	pnlsb_info->ifree_bitmap[0] = cpu_to_le64(0xfffffffffffffffc);	
 
-	//we don't know how to copy bfree_bitmap from disk so we do this :(
+	//we don't know how to copy bfree_bitmap from disk so we do this 
 	uint64_t mask, line;
 	uint32_t nr_used = pnlsb_info->nr_istore_blocks +
 		pnlsb_info->nr_ifree_blocks +
@@ -427,10 +462,8 @@ static int pnlfs_fill_super(struct super_block *sb, void *d, int silent)
 		i++;
 	}
 
-	//where is the bitmaps?
-	//-inside le bloc after sb and nr_istore_blocks
-	//-we have to calculate to know where is the bloc of bitmaps
 /*
+	//we have tried to do this to copy the bitmaps
 	for (i = 0; i < pnlsb_info->nr_ifree_blocks; i++){
 		if (!(bh = sb_bread(sb, pnlsb->nr_istore_blocks + 1 + i))) {
 			pr_info(KERN_INFO "¯\\_(ツ)_/¯ unable to read superblock");
@@ -449,7 +482,7 @@ static int pnlfs_fill_super(struct super_block *sb, void *d, int silent)
 	//récupérer l’inode racine du disque (inode 0)	
 	root = pnlfs_iget(sb, 0);
 	if (IS_ERR(root)) {
-		pr_info(KERN_INFO "¯\\_(ツ)_/¯ get root inode failed\n");
+		pr_info(KERN_INFO "¯\\_(ツ)_/¯ [pnlfs_fill_super] get root inode failed\n");
 		return PTR_ERR(root);
 	}
 	//définir ses propriétaires
@@ -457,10 +490,11 @@ static int pnlfs_fill_super(struct super_block *sb, void *d, int silent)
 	//puis la déclarer comme inode racine
 	sb->s_root = d_make_root(root);
 	if (!(sb->s_root)) {
-		pr_info(KERN_INFO "¯\\_(ツ)_/¯ get root dentry failed\n");
+		pr_info(KERN_INFO "¯\\_(ツ)_/¯ [pnlfs_fill_super] get root dentry failed\n");
 		return -ENOMEM;
 	}
 	return 0;
+
 error:
 	sb->s_fs_info = NULL;
 	if(pnlsb_info)
@@ -470,25 +504,16 @@ error:
 	return ret;
 }
 
-/*
-The mount() method must return the root dentry of the tree requested by
-caller.  An active reference to its superblock must be grabbed and the
-superblock must be locked.  On failure it should return ERR_PTR(error).
-
-The get_sb_bdev( ) VFS function allocates and initializes a new superblock suitable for disk-based
-filesystems ; it receives the address of the ext2_fill_super( ) function, which reads the disk
-superblock from the Ext2 disk partition.
-*/
 static struct dentry *pnlfs_mount(struct file_system_type *fs_type,
 	int flags, const char *dev_name, void *data)
 {
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) pnlfs_mount\n");
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_mount]\n");
 	return mount_bdev(fs_type, flags, dev_name, data, pnlfs_fill_super);
 }
 
 static void kill_pnlfs_super(struct super_block *s)
 {
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) kill_block_super\n");
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [kill_block_super]\n");
 	kill_block_super(s);
 }
 
@@ -499,8 +524,6 @@ static struct file_system_type pnlfs_type = {
 	.kill_sb = kill_pnlfs_super,
 };
 MODULE_ALIAS_FS("pnl_VFS");
-
-
 
 static void pnlfs_init_once(void *p)
 {
@@ -521,15 +544,15 @@ static int pnlfs_init(void)
 	if (pnl_inode_cachep == NULL)
 		goto error_kmem_cache_create;
 
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) module loaded\n");
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_init] module loaded\n");
 	return 0;
-/*ERROR HANDLE*/
+
 error_kmem_cache_create:
-	pr_info(KERN_INFO "¯\\_(ツ)_/¯ kmem_cache_create failed");
+	pr_info(KERN_INFO "¯\\_(ツ)_/¯ [pnlfs_init] kmem_cache_create failed");
 	unregister_filesystem(&pnlfs_type);
 	return -ENOMEM;
 error_register_filesystem:
-	pr_info(KERN_INFO "¯\\_(ツ)_/¯ register_filesystem failed");
+	pr_info(KERN_INFO "¯\\_(ツ)_/¯ [pnlfs_init] register_filesystem failed");
 	return -1;
 }
 module_init(pnlfs_init);
@@ -537,6 +560,6 @@ module_init(pnlfs_init);
 static void pnlfs_exit(void)
 {
 	unregister_filesystem(&pnlfs_type);
-	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) module unloaded\n");
+	pr_info(KERN_INFO "( ͡° ͜ʖ ͡°) [pnlfs_exit] module unloaded\n");
 }
 module_exit(pnlfs_exit);
